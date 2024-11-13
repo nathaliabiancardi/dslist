c0f6402a263525c84cd8591733bbf3e5f3252aa6
nathalia biancardi <natybiancardi@hotmail.com>
2024-11-13 16:48:10 -0300

    Project created

---
+++ src/main/java/com/dslist/demodslist/DemodslistApplication.java
@@ -0,0 +1,13 @@
+package com.dslist.demodslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodslistApplication.class, args);
+	}
+
+}
